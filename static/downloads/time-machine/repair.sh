d2ede5196d8a0ebcc96c48b5623d5316dcb9a1a4
Tony Lawrence <code@tonylawrence.com>
2017-03-05 15:47:43 +0000

    added download for repair script

--- static/downloads/time-machine/repair.sh
+++ static/downloads/time-machine/repair.sh
@@ -1,1 +1,49 @@
-echo hello
+if [[ $(whoami) != 'root' ]]; then
+    exit 1
+fi
+
+read -p 'Enter Time Machine Hostname: ' HOSTNAME
+read -p 'Enter Share: ' SHARE
+read -p 'Enter Username: ' USERNAME
+read -s -p 'Enter Password: ' PASSWORD
+
+TM_NAME=$(hostname -s | sed -e 's/-/ /g')
+MOUNT=/Volumes/TimeMachine
+SPARSEBUNDLE=$MOUNT/$TM_NAME.sparsebundle
+PLIST=$SPARSEBUNDLE/com.apple.TimeMachine.MachineID.plist
+
+echo "Disabling Time Machine"
+tmutil disable
+
+echo "Mounting volume"
+mkdir $MOUNT
+mount_afp afp://$USERNAME:$PASSWORD@$HOSTNAME/$SHARE $MOUNT
+
+echo "Changing file and folder flags"
+chflags -R nouchg "$SPARSEBUNDLE"
+
+echo "Attaching sparse bundle"
+DISK=`hdiutil attach -nomount -readwrite -noverify -noautofsck "$SPARSEBUNDLE" | grep Apple_HFS | cut -f 1`
+
+echo "Repairing volume"
+#diskutil repairVolume $DISK
+/sbin/fsck_hfs -fry $DISK
+
+echo "Fixing Properties"
+cp "$PLIST" "$PLIST.backup"
+sed -e '/RecoveryBackupDeclinedDate/{N;d;}'   \
+    -e '/VerificationState/{n;s/2/0/;}'       \
+    "$PLIST.backup" \
+    > "$PLIST"
+
+echo "Unmounting volumes"
+hdiutil detach /dev/$DISK
+umount $MOUNT
+
+echo "Enabling Time Machine"
+tmutil enable
+
+echo "Starting backup"
+tmutil startbackup
+
+exit 0
